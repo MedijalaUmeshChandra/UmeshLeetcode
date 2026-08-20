bc4cf8358ce997aec18ee9d97dc3a322b8887b0f
UmeshChandra <umeshmedijala@gmail.com>
2026-08-20 16:43:30 +0530

    Time: 0 ms (100%), Space: 21.3 MB (18.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,20 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-
-        n = len(nums)
-        
-        ans = n/2
-        dici = {}
-        for i in range(n):
-            val = nums[i]
-            if val not in dici:
-                dici[val] = 1
-            else:
-        
-                dici[val] += 1
-
-
-            if dici[val] > ans:
-                return val
-
+        nums.sort()
+        n = len(nums)//2
+        return nums[n]
         
